e6efa7976cdd35ca7d0f722f41aca3433b2ef4d2
graupv <38445832+graupv@users.noreply.github.com>
2020-05-27 01:22:55 -0600

    vulcanizados js y casi db

--- src/js/formularios.js
+++ src/js/formularios.js
@@ -1,80 +1,124 @@
 const remote = require('electron').remote
-const root = document.getElementById('root');
+
+const Sequelize = require('sequelize')
+const { QueryTypes } = require('sequelize')
+
+// var db_config ={
+//     host: 'localhost',
+//     user: 'root',
+//     passwor: 'MySQL', //CAMBIAR ESTA CONTRASEÑA
+//     port: 3306,
+//     database: 'users'
+// }
+
+var db_config = {
+    host: 'localhost',
+    user: 'groot',
+    passwor: '7654321.', //CAMBIAR ESTA CONTRASEÑA
+    port: 3306,
+    database: 'prensas'
+}
+
+var sequelize = new Sequelize('mysql://'+db_config.user+':'+db_config.passwor
++'@'+db_config.host + ':' + db_config.port + '/' + db_config.database + '')
+
+sequelize.authenticate().then(()=>{
+    console.log('=========================================')
+    console.log('database : ' + db_config.database + 'connected')
+    console.log('=========================================')
 
 
-const formularios = ['Laminado de Calandra', 'Troquelado de Tela', 'Pesado de Formulas', 'Vulcanizado de Suela', ];
+}).catch((err) => {
+    console.log('=========================================')
+    console.log('error connecting : ' + err)
+    console.log('=========================================')
+})
 
-const render = (mount) => {
-    mount.innerHTML = '';
-    const header = document.createElement('div');
-    header.setAttribute('class','header');
-    header.style.width = '100%';
-    header.innerText = 'Formularios';
-    mount.appendChild(header);
-
-    const select = document.createElement('select');
-    select.setAttribute('class', 'dropdown');
-    select.setAttribute('id', 'select');
-    select.setAttribute('onchange', 'changeFormulario(this)');
-    
-    let options = formularios.map(x => {
-        let b = document.createElement('option');
-        b.setAttribute('value', x);
-        b.innerText = x;
-        return b;
-    });
-
-    options.forEach(element => {
-        select.appendChild(element);
-    });
-
-    mount.appendChild(select);
-
-};
-const changeFormulario = (element) => {
-
-
-    switch(element.value) {
-        case 'Laminado de Calandra': {
-            console.log('Laminado de Calandra');
-            break;
-        }
-        case 'Troquelado de Tela': {
-            console.log('Troquelado de Tela');
-            break;
-        }
-        case 'Pesado de Formulas': {
-            console.log('Pesado de Formulas');
-            break;
-        }
-        case 'Vulcanizado de Suela': {
-            console.log('Vulcanizado de Suela');
-            break;
-        }
-
+const vulcanizados = sequelize.define('vulcanizado',{
+    id: { // numero_formulario
+        type: Sequelize.INTEGER
+    },
+    operador_id:{
+        type: Sequelize.INTEGER
+    },
+    prensa_id:{
+        type: Sequelize.INTEGER
+    },
+    producto_id:{
+        type: Sequelize.INTEGER
+    },
+    cargas_program:{
+        type: Sequelize.INTEGER
+    },
+    tiempo_real:{
+        type: Sequelize.INTEGER
+    },
+    tiempo_no_utilizado:{
+        type: Sequelize.INTEGER
+    },
+    buen_estado:{
+        type: Sequelize.INTEGER
+    },
+    semana:{
+        type: Sequelize.INTEGER
+    },
+    mes:{
+        type: Sequelize.INTEGER
+    },
+    producto_defecto:{
+        type: Sequelize.INTEGER
+    },
+    faltante_cargas:{
+        type: Sequelize.INTEGER
+    },
+    mes:{
+        type: Sequelize.INTEGER
+    },
+    type:{
+        type: Sequelize.STRING
     }
-};
+})
+
+
+vulcanizados.sync()
+
 
 const getValues = () => ({
     numero_formulario : document.getElementById('numero-formulario').value,
-    tipo_formulario : document.getElementById('formSelect').value,
     fecha : document.getElementById('fecha').value,
     semana : document.getElementById('semana').value,
     mes : document.getElementById('mes').value,
-    codigo_operador : document.getElementById('codigo-operador').value,
-    codigo_producto : document.getElementById('codigo-producto').value,
-
-
+    codigo_operador : document.getElementById('operador-id').value,
+    codigo_producto : document.getElementById('producto-id').value,
+    codigo_prensa : document.getElementById('prensa-id').value,
+    cargas_program : document.getElementById('cargas-program').value,
+    producto_defecto : document.getElementById('producto-defecto').value,
+    buen_estado : document.getElementById('buen-estado').value,
+    total_cargas : document.getElementById('total-cargas').value,
+    cargas_faltantes : document.getElementById('cargas-faltantes').value,
+    // fin primera col.
+    porcentaje_cump_total : document.getElementById('porcentaje_cump_total').value, // double
+    porcentaje_buen_estado : document.getElementById('porcentaje_buen_estado').value, // double
+    paro_id : document.getElementById('paro').value,
+    peso_producir : document.getElementById('peso_producir').value, // decimal
+    peso_producido : document.getElementById('peso_producido').value,// decimal
+    faltante : document.getElementById('faltante').value, // decimal
+    rebaba : document.getElementById('rebaba').value, // decimal
+    cumplimiento_kg : document.getElementById('cumplimiento_kg').value, // double
+    tiempo_real : document.getElementById('tiempo_real').value, // int
+    tiempo_no_utilizado : document.getElementById('tiempo_no_utilizado').value, // int
+    kwh : document.getElementById('kwh').value, // int
 });
 
 const printValues = () => {
-    console.log(getValues());
+    values = getValues();
+
 }
 
 const formButton = document.getElementById('form-button');
-console.log('hey');
+
 formButton.setAttribute('onclick', 'printValues()');
-console.log('ho');
+
 
 
 // render(root);
@@ -84,7 +128,3 @@ function exitFromApp(){
     w.close();
 }
 
-/**
- * query para pre llenar formulario
- * query para guardar la info del formulario segun tipo de formulario
- */
--- src/js/mainmenu.js
+++ src/js/mainmenu.js
@@ -1,19 +1,8 @@
-const root = document.getElementById('root');
-const ingbut = document.getElementById('ingreso-manual');
-ingbut.style.color('black');
+
+
 
 const remote = require('electron').remote;
 
-const render = (mount) => {
-    mount.innerHTML = '';
-    const contenedorBoard = document.createElement('div');
-    contenedorBoard.setAttribute('class','board');
-    contenedorBoard.style.width = '550px';
-    contenedorBoard.style.height = '550px';
-    mount.appendChild(contenedorBoard);
-};
-
-render(root);
 
 let w = remote.getCurrentWindow()
 function exitFromApp(){
